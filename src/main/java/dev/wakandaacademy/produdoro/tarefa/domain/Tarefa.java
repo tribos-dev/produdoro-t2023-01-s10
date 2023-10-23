28772d71b7872e3bb68d05929d854d5a8297fe34
wanderson648 <testepedroca@gmail.com>
2023-10-23 17:28:06 -0300

    implementa metodo ativarTarefa e camada API

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
@@ -55,4 +55,9 @@ public class Tarefa {
 			throw APIException.build(HttpStatus.UNAUTHORIZED, "Usuário não é dono da Tarefa solicitada!");
 		}
 	}
+
+	public void ativarTarefa() {
+		this.statusAtivacao = StatusAtivacaoTarefa.ATIVA;
+	}
+
 }
