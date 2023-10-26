423cda636568270ab882e83cff53c8adc4b38886
GeorgeSantos7 <georgedossantosramos@gmail.com>
2023-10-26 20:34:03 -0300

    inicia teste unitario

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,7 +1,25 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+
+@ExtendWith(MockitoExtension.class)
 class UsuarioApplicationServiceTest {
+
+	//	@Autowired
+    @InjectMocks
+    UsuarioApplicationService usuarioApplicationService;
+
+    //	@MockBean
+    @Mock
+    UsuarioRepository usuarioRepository;
+    
+    void deveMudarStatusPausaLonga() {
+    	
+    }
 
 }
